920fb264cab47758933111cf25470d76f2c5a113
kaiomagalhaes <contato@kaiomagalhaes.com.br>
2015-06-05 12:32:19 -0300

    add search and parsers for data in ppagronegocio.herokuapp.com/json/graph-index-price.json

--- app/src/main/java/com/kty/kaio/ppagronegocio_android/MainActivityFragment.java
+++ app/src/main/java/com/kty/kaio/ppagronegocio_android/MainActivityFragment.java
@@ -1,10 +1,24 @@
 package com.kty.kaio.ppagronegocio_android;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 /**
@@ -18,6 +32,91 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        FetchDataTask task = new FetchDataTask();
+        task.execute();
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    private String[] getDataFromJson(String forecastJsonStr)
+            throws JSONException {
+        JSONObject forecastJson = new JSONObject(forecastJsonStr);
+        JSONArray weatherArray = forecastJson.getJSONArray("months");
+        JSONObject lastMonth = weatherArray.getJSONObject(weatherArray.length() - 1);
+        JSONArray products = lastMonth.getJSONArray("products");
+        String month = lastMonth.getString("month");
+
+        System.out.println(products);
+        String[] resultStrs = new String[products.length()+2];
+        resultStrs[0] = month;
+        for (int i = 0; i < products.length(); i++) {
+            JSONObject product = products.getJSONObject(i);
+            String name = product.getString("name");
+            String priceIndex = product.getString("priceIndex");
+            resultStrs[i + 1] = name + " => " + priceIndex;
+        }
+
+        for (String s : resultStrs) {
+            Log.v("WeaterApp", "Forecast entry: " + s);
+        }
+
+        return resultStrs;
+    }
+
+    public class FetchDataTask extends AsyncTask<String, Void, String[]> {
+
+        @Override
+        protected String[] doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String json = null;
+            try {
+                String BASE_URL = "http://ppagronegocio.herokuapp.com/json/graph-index-price.json";
+                Uri uri = Uri.parse(BASE_URL);
+                URL url = new URL(uri.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+                json = buffer.toString();
+                Log.v("PpagronegocioWebservice", "JSON:" + json);
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getDataFromJson(json);
+            } catch (JSONException e) {
+                System.out.println("Error parsing json");
+            }
+
+            return null;
+        }
     }
 }
